ccff0f772013cbf4d3a4d56cb30ab1e1c8c0a67a
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-09-09 01:14:54 -0400

    Build form skeleton in HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 console.log("Hey there");
 
 const sidebar = document.querySelector(".sidebar");
-const openBtn = document.getElementById("open-btn");
-const closeBtn = document.getElementById("close-btn");
+const openSidebar = document.getElementById("open-sidebar");
+const closeSidebar = document.getElementById("close-sidebar");
 
 function toggleNav() {
 	sidebar.classList.toggle("open");
 }
 
-openBtn.addEventListener("click", toggleNav);
-closeBtn.addEventListener("click", toggleNav);
+openSidebar.addEventListener("click", toggleNav);
+closeSidebar.addEventListener("click", toggleNav);
+
+// events.js
+const inquireBtns = document.querySelectorAll(".inquire");
+const closeModal = document.getElementById("close-modal");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleFormSubmit);
+function handleFormSubmit(e) {
+	e.preventDefault();
+	console.log("Form submitted.");
+}
+
+inquireBtns.forEach((button) => {
+	button.addEventListener("click", toggleModal);
+});
+
+closeModal.addEventListener("click", toggleModal);
+
+function toggleModal() {
+	const modal = document.querySelector(".modal");
+	modal.classList.toggle("hidden");
+}
